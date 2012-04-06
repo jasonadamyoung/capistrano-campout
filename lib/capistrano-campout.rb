e1c3607013997bf15038b70f49c28ba35be2a121
Jason Adam Young <jasonadamyoung@gmail.com>
2012-04-06 11:05:44 -0400

    initial commit of capistrano-campout from 'bundle gem'

---
+++ lib/capistrano-campout.rb
@@ -0,0 +1,7 @@
+require "capistrano-campout/version"
+
+module Capistrano
+  module Campout
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano-campout/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Campout
+    VERSION = "0.0.1"
+  end
+end
